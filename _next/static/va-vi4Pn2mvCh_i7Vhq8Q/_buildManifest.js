285e8472ac59753994bb12d6edf0b72df07b8df0
lorey <git@karllorey.com>
2022-07-21 18:36:46 +0000

    Deploying to gh-pages from @ apistemic/apistemic.com@49212cd52e74d9bbca072346380c76216ffb3235 🚀

---
+++ _next/static/va-vi4Pn2mvCh_i7Vhq8Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-12f4ef2b153ccd1c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
