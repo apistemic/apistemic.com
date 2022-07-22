26156a0f9849e2c4ad9a43b33a2ab83ab15ab275
Karl Lorey <git@karllorey.com>
2022-07-22 09:31:01 +0200

    Update claim

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
               <div>
                   <Image src="/logo-squared.png" alt="Logo of apistemic" height="100" width="100" />
                   <h1>apistemic</h1>
-                  <h2>power your business with data-driven insights</h2>
+                  <h2>access the world's data</h2>
               </div>
               <div className="mt-12 text-sm">
                   © Copyright 2022, <a href="https://loreyventures.com">Lorey Ventures</a>.
